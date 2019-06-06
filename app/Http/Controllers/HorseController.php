a8dbf7ed91032a4694eb36c2381483bf71441766
JotaVe <nicolas.steck@hotmail.com>
2019-06-06 17:30:32 -0300

    criado o arquivo HorseController

---
+++ app/Http/Controllers/HorseController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HorseController extends Controller
+{
+    public function listarCavalos(){
+
+    }
+
+    public function cadastrarCavalos(){
+        
+    }
+
+    public function deletarCavalos(){
+        
+    }
+}
